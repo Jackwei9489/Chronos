84e95efe1c2204df511fc40720653e801c8cf1fa
KevinAmani <15861800534@163.com>
2017-06-10 14:50:57 +0800

    u

--- src/app/service/UserService.ts
+++ src/app/service/UserService.ts
@@ -1,8 +1,8 @@
 /**
  * Created by wz on 2017/6/9.
  */
+import {Http, RequestOptions, Headers} from '@angular/http';
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
 import {Comment} from '../models/Comment'
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -15,7 +15,11 @@ export class UserService {
   }
 
   addComment(comment: Comment): Observable<ResultModel> {
-    const uri = 'xx'; // TODO
-    return this.http.post(serverURL + uri, comment).map(response => response.json());
+  	const headers=new Headers({
+  		'Content-Type': 'application/x-www-form-urlencoded'
+  	});
+  	const options=new RequestOptions({headers:headers,body:comment});
+    const uri = 'http://127.0.0.1:8000/message'; // TODO
+    return this.http.post(serverURL + uri, comment,options).map(response => response.json());
   }
 }
