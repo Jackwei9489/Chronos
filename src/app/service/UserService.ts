a9582d4ad1fda61498be61b5623770ae9d8dc825
Jackwei9489 <376528106@qq.com>
2017-06-10 14:48:27 +0800

    post data as form data instead of a request payload

--- src/app/service/UserService.ts
+++ src/app/service/UserService.ts
@@ -2,20 +2,23 @@
  * Created by wz on 2017/6/9.
  */
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, RequestOptions, Headers} from '@angular/http';
 import {Comment} from '../models/Comment'
-import { Observable } from 'rxjs/Observable';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import {ResultModel} from '../models/ResultModel';
 
-const serverURL = ''; // TODO
+const serverURL = 'http://192.168.31.36:8000/'; // TODO
+declare let $: any;
 @Injectable()
 export class UserService {
   constructor(private http: Http) {
   }
 
   addComment(comment: Comment): Observable<ResultModel> {
-    const uri = 'xx'; // TODO
-    return this.http.post(serverURL + uri, comment).map(response => response.json());
+    const headers = new Headers({'Content-Type': 'application/x-www-form-urlencoded'});
+    const options = new RequestOptions({headers: headers});
+    const uri = 'search'; // TODO
+    return this.http.post(serverURL + uri, $.param(comment), options).map(response => response.json() as ResultModel);
   }
 }
